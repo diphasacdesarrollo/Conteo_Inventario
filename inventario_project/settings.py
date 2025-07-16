17fc1442c51eee439d6a225183dc022b929aa11e
diphasacdesarrollo <diphasacdesarrollo@gmail.com>
2025-07-16 09:16:48 -0500

    UpDate Settings render.yaml

--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -115,3 +115,4 @@ STATICFILES_DIRS = [BASE_DIR / "static"] if DEBUG else []
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
