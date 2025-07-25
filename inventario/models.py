256643c86a8f3eabaebc5c980fd2f2f06600bcbf
diphasacdesarrollo <diphasacdesarrollo@gmail.com>
2025-07-25 10:35:23 -0500

    Campo codigo agregado correctamente a Producto

--- inventario/models.py
+++ inventario/models.py
@@ -2,10 +2,11 @@
 from django.db import models
 
 class Producto(models.Model):
+    codigo = models.CharField(max_length=100)
     nombre = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.codigo} - {self.nombre}"
 
 class Lote(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name='lotes')
